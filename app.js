711571aef0fed8203e51cd76c6e884eefafe6b6e
Keishi Kawada <megumisheee@gmail.com>
2018-01-10 16:48:53 +0900

    some fixed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/sw.js").then(function(registration) {
+    navigator.serviceWorker.register("sw.js").then(function(registration) {
         console.log('ServiceWorker registration successfull with scope: ', registration.scope);
     }).catch(function(err) {
         console.log('ServiceWorker registration failed: ', err);
